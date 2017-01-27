d3d96f9b7a9086fac5a591eaced00e1a26c35268
kavyabvishwanath <kavyabvishwanath25@gmail.com>
2017-01-27 16:51:13 +0530

    added exception handling

--- gitrepos.py
+++ gitrepos.py
@@ -1,5 +1,8 @@
+## this script gets top N repos for given organisation org
+
 from urllib2 import Request, urlopen, URLError
 import json
+import sys
 
 def getReposURL(baseURL,org):
         return baseURL+'orgs/'+org+'/repos'
@@ -11,17 +14,22 @@ def getResponse(URL):
         except URLError, e:
                 print 'Error fetching Repos: ',e
 
+## main module which returns the top N repos for given organisation
 def gettopNrepos(baseURL,org,topn):
-        topNrepos={}
-        topForks=[0,0,0,0,0]
+        topNrepos={} #holds the top r repos
+        topForks=[0 for i in range(0,topn)] #holds the top repo counts at any point we maintain state of only required top number of top repos
         reposURL=getReposURL(baseURL,org)
         reposList=getResponse(reposURL)
+        if not reposList:
+            print 'No repositories found for ',org
+            sys.exit(2)
         for repo in reposList:
                 repoName=repo['name']
                 repoForkCount=repo['forks_count']
                 curMinForks=min(topForks)
                 curMinForksIndex=topForks.index(curMinForks)
                 if curMinForks <= repoForkCount:
+                        #base case
                         if any(topNrepos)==False or len(topNrepos) < topn:
                                 topNrepos[repoName]=repoForkCount
                                 topForks[curMinForksIndex]=repoForkCount
